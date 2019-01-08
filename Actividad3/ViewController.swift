4de646f1bb255a6d0ace025f47429a397e6321bc
usuario <usuario@macbook-pro.local>
2019-01-08 14:19:22 -0600

    Se agrega funcionalidad para calcular propina del quince y veinte por ciento

--- Actividad3/ViewController.swift
+++ Actividad3/ViewController.swift
@@ -18,14 +18,33 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     @IBAction func calcularDiez(_ sender: UIButton) {
-        let montoTotalFl: Float? = Float(montoTotal.text!)
-        let montoPropina: Float?  = montoTotalFl! * 0.1
-        let montoTotalMasPropina: Float? = Float(montoTotalFl! + montoPropina!)
-        calculo!.text = "Total a Pagar : \(montoTotalMasPropina)"
+        let montoTotalFl: Float! = Float(montoTotal.text!)
+        let montoPropina: Float!  = montoTotalFl! * 0.1
+        let montoTotalMasPropina: Float! = Float(montoTotalFl! + montoPropina!)
+        calculo!.text = "Total a Pagar : $\(montoTotalMasPropina ?? 0.0)"
         
     }
+    
+    @IBAction func calcularQuince(_ sender: UIButton) {
+        let montoTotalFl: Float? = Float(montoTotal.text!)
+        let montoPropina: Float?  = montoTotalFl! * 0.15
+        let montoTotalMasPropina: Float? = Float(montoTotalFl! + montoPropina!)
+        calculo!.text = "Total a Pagar : $\(montoTotalMasPropina ?? 0.0)"
+    }
+    
+    @IBAction func calcularVeinte(_ sender: UIButton) {
+        let montoTotalFl: Float? = Float(montoTotal.text!)
+        let montoPropina: Float?  = montoTotalFl! * 0.2
+        let montoTotalMasPropina: Float? = Float(montoTotalFl! + montoPropina!)
+        calculo!.text = "Total a Pagar : $\(montoTotalMasPropina ?? 0.0)"
+    }
+    
+    @IBAction func calcularPropina(_ sender: UIButton) {
+        print("nada")
+    }
+    
     
 }
 
